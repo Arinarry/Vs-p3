3a01a172a33f46539c9ae0ffde8350aa5a2fb3c7
IlyaPozhidaev <99234640+IlyaPozhidaev@users.noreply.github.com>
2023-03-05 23:55:41 +0700

    Create MyAPP

---
+++ ItemsControlWrapPanelExample/Models/CityEvent.cs
@@ -0,0 +1,87 @@
+﻿using DynamicData.Binding;
+using System.Xml.Serialization;
+using System;
+using static ItemsControlWrapPanelExample.Models.CityEvent;
+
+namespace ItemsControlWrapPanelExample.Models
+{
+    [Serializable]
+    [XmlRoot("CityEvents", Namespace = "")]
+    public class CityEvent : AbstractNotifyPropertyChanged
+    {
+        private string _Header = "";
+
+        private string _Description = "Описание отсутствует";
+
+        private string _Image = "";
+
+        private string _Date = "";
+
+        private category _Category;
+
+        private double _Price = 0;
+        public string Header
+        {
+            get => _Header;
+            set => SetAndRaise(ref _Header, value);
+
+        }
+        public string Date
+        {
+            get => _Date;
+            set => SetAndRaise(ref _Date, value);
+        }
+        private string CheckStringLenght(string? str)
+        {
+            const int maxLength = 135;
+
+            if (!string.IsNullOrEmpty(str) && str.Length > maxLength)
+            {
+                str = str[..(maxLength - 2)] + "...";
+            }
+
+            return str;
+        }
+        public string Description
+        {
+            get => _Description;
+            set => SetAndRaise(ref _Description, CheckStringLenght(value));
+        }
+        public double Price
+        {
+            get => _Price;
+            set => SetAndRaise(ref _Price, value);
+        }
+        public enum category
+        {
+            [XmlEnum("Дети")]
+            Children,
+            [XmlEnum("Спорт")]
+            Sport,
+            [XmlEnum("Культура")]
+            Culture,
+            [XmlEnum("Экскурсия")]
+            Excursion,
+            [XmlEnum("Образ жизни")]
+            Lifestyles,
+            [XmlEnum("Вечеринка")]
+            Party,
+            [XmlEnum("Обучение")]
+            Education,
+            [XmlEnum("Онлайн")]
+            Online,
+            [XmlEnum("Шоу")]
+            Show
+        }
+        public category Category
+        {
+            get => _Category;
+            set => SetAndRaise(ref _Category, value);
+        }
+        public string Image
+        {
+            get => _Image;
+            set => SetAndRaise(ref _Image, value);
+        }
+    }
+}
---
+++ ItemsControlWrapPanelExample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using ItemsControlWrapPanelExample.Models;
+using ReactiveUI;
+using System.Collections.ObjectModel;
+using System.Xml;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ItemsControlWrapPanelExample.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private List<CityEvent> _items;
+
+        public List<CityEvent> Items
+        {
+            get { return _items; }
+            set { this.RaiseAndSetIfChanged(ref _items, value); }
+        }
+        public ObservableCollection<CityEvent> ChildrenEvents { get; set; }
+        public ObservableCollection<CityEvent> SportEvents { get; set; }
+        public ObservableCollection<CityEvent> CultureEvents { get; set; }
+        public ObservableCollection<CityEvent> ExcursionEvents { get; set; }
+        public ObservableCollection<CityEvent> LifestyleEvents { get; set; }
+        public ObservableCollection<CityEvent> PartyEvents { get; set; }
+        public ObservableCollection<CityEvent> EducationEvents { get; set; }
+        public ObservableCollection<CityEvent> OnlineEvents { get; set; }
+        public ObservableCollection<CityEvent> ShowEvents { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Items = CityEventDatabase.Load();
+            ChildrenEvents = new ObservableCollection<CityEvent>(Items.Where(e => e.Category == CityEvent.category.Children));
+            SportEvents = new ObservableCollection<CityEvent>(Items.Where(e => e.Category == CityEvent.category.Sport));
+            CultureEvents = new ObservableCollection<CityEvent>(Items.Where(e => e.Category == CityEvent.category.Culture));
+            ExcursionEvents = new ObservableCollection<CityEvent>(Items.Where(e => e.Category == CityEvent.category.Excursion));
+            LifestyleEvents = new ObservableCollection<CityEvent>(Items.Where(e => e.Category == CityEvent.category.Lifestyles));
+            PartyEvents = new ObservableCollection<CityEvent>(Items.Where(e => e.Category == CityEvent.category.Party));
+            EducationEvents = new ObservableCollection<CityEvent>(Items.Where(e => e.Category == CityEvent.category.Education));
+            OnlineEvents = new ObservableCollection<CityEvent>(Items.Where(e => e.Category == CityEvent.category.Online));
+            ShowEvents = new ObservableCollection<CityEvent>(Items.Where(e => e.Category == CityEvent.category.Show));
+        }
+    }
+}
